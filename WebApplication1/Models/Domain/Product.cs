41c9a3135c1e4978a66dc2f2bbdf7641db0cbfd6
NadejdaChitakova <nadejda.chitakova13@gmail.com>
2023-01-03 12:58:45 +0200

    Created new tables

---
+++ WebApplication1/Models/Domain/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication1.Models.Domain
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal BuyPrice { get; set; }
+        public decimal SellPrice { get; set; }
+        public string PathFile { get; set; }
+        public string CategoryId { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Entity/RegisterDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models.Entity
+{
+    public class RegisterDTO
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+    }
+}
